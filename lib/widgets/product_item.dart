bda478d2410d29a5e8d0f04080e6af31910674ea
Ronaldo Bicca <kuka@MacBook-de-Ronaldo.local>
2019-08-12 21:11:32 -0300

    Unit 212 started

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -13,7 +13,6 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final product = Provider.of<Product>(context, listen: false);
     final cart = Provider.of<Cart>(context, listen: false);
 
@@ -48,6 +47,13 @@ class ProductItem extends StatelessWidget {
             icon: Icon(Icons.shopping_cart),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text('Item added to cart'),
+                duration: Duration(seconds: 2),
+                action: SnackBarAction(label: 'Undo',onPressed: (){
+                  
+                },),
+              ));
             },
             color: Theme.of(context).accentColor,
           ),
